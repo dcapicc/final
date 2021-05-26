f822f266932f874ca7e541f28880205e54269760
Dante Capicchioni <dante.capicchioni@kellogg.northwestern.edu>
2021-05-25 21:15:51 -0500

    firebase added

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyCCABR8tEHrxP280JMczhIiuw8Fh3Lgr30",
+  authDomain: "kiei-final-project-dbc73.firebaseapp.com",
+  projectId: "kiei-final-project-dbc73",
+  storageBucket: "kiei-final-project-dbc73.appspot.com",
+  messagingSenderId: "985516584286",
+  appId: "1:985516584286:web:7d90bf95ebd5ad0164f3cf"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
